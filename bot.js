deeb93135161d63e09f1766068e854f011bbacfc
Lennon McLean <lmmclean08@gmail.com>
2020-08-10 08:42:46 -0400

    add disconnect on stop and target not found.

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var args = process.argv;
 var http = require('@thecoder08/http');
+console.log('Type "stop" to disconnect before exiting.');
 http.request({
   hostname: args[3],
   port: args[4],
@@ -9,6 +10,7 @@ http.request({
 setInterval(function() {
   var movement = Math.floor(Math.random() * 5);
   if (movement == 0) {
+    console.log('Moving up...');
     http.request({
       hostname: args[3],
       port: args[4],
@@ -17,6 +19,7 @@ setInterval(function() {
     }, function(data) {});
   }
   if (movement == 1) {
+    console.log('Moving down...');
     http.request({
       hostname: args[3],
       port: args[4],
@@ -25,6 +28,7 @@ setInterval(function() {
     }, function(data) {});
   }
   if (movement == 2) {
+    console.log('Moving left...');
     http.request({
       hostname: args[3],
       port: args[4],
@@ -33,6 +37,7 @@ setInterval(function() {
     }, function(data) {});
   }
   if (movement == 3) {
+    console.log('Moving right...');
     http.request({
       hostname: args[3],
       port: args[4],
@@ -41,6 +46,7 @@ setInterval(function() {
     }, function(data) {});
   }
   if (movement == 4) {
+    console.log('Shooting at target...');'
     http.request({
       hostname: args[3],
       port: args[4],
@@ -48,12 +54,32 @@ setInterval(function() {
       method: 'GET'
     }, function(data) {
       var serverdata = JSON.parse(data);
-      http.request({
-        hostname: args[3],
-        port: args[4],
-        path: '/shoot?name=' + encodeURI(args[5]) + '&x=' + serverdata[args[6]].x + '&y=' + serverdata[args[6]].y,
-        method: 'GET'
-      }, function(data) {});
+      if (serverdata.hasOwnProperty(args[6])) {
+        http.request({
+          hostname: args[3],
+          port: args[4],
+          path: '/shoot?name=' + encodeURI(args[5]) + '&x=' + serverdata[args[6]].x + '&y=' + serverdata[args[6]].y,
+          method: 'GET'
+        }, function(data) {});
+      }
+      else {
+        console.log('Target not found.');
+      }
     });
   }
 }, 1000);
+process.stdin.on('data', function(data) {
+  if (data.toString() == ('stop' + require('os').EOL)) {
+    console.log('Stopping...');
+    http.request({
+      hostname: args[3],
+      port: args[4],
+      path: '/leave?name=' + encodeURI(args[5]),
+      method: 'GET',
+    }, function(data) {});
+    process.exit();
+  }
+  else {
+    console.log('I don\'t understand.');
+  }
+});
