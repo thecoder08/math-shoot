38555e0c9dfcc93e2fee8236cb82913c8d4067ea
Lennon McLean <lmmclean08@gmail.com>
2020-08-10 09:20:17 -0400

    add auto-joining ability

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 var args = process.argv;
 var http = require('@thecoder08/http');
 console.log('Type "stop" to disconnect before exiting.');
-http.request({
-  hostname: args[3],
-  port: args[4],
-  path: '/join?name=' + encodeURI(args[5]),
-  method: 'GET'
-}, function(data) {});
+function join() {
+  http.request({
+    hostname: args[3],
+    port: args[4],
+    path: '/join?name=' + encodeURI(args[5]),
+    method: 'GET'
+  }, function(data) {});
+}
+join();
 setInterval(function() {
   var movement = Math.floor(Math.random() * 5);
   if (movement == 0) {
@@ -16,7 +19,11 @@ setInterval(function() {
       port: args[4],
       path: '/up?name=' + args[5],
       method: 'GET'
-    }, function(data) {});
+    }, function(data) {
+      if (data == 'join the server to move') {
+        join();
+      }
+    });
   }
   if (movement == 1) {
     console.log('Moving down...');
@@ -25,7 +32,11 @@ setInterval(function() {
       port: args[4],
       path: '/down?name=' + args[5],
       method: 'GET'
-    }, function(data) {});
+    }, function(data) {
+      if (data == 'join the server to move') {
+        join();
+      }
+    });
   }
   if (movement == 2) {
     console.log('Moving left...');
@@ -34,7 +45,11 @@ setInterval(function() {
       port: args[4],
       path: '/left?name=' + args[5],
       method: 'GET'
-    }, function(data) {});
+    }, function(data) {
+      if (data == 'join the server to move') {
+        join();
+      }
+    });
   }
   if (movement == 3) {
     console.log('Moving right...');
@@ -43,7 +58,11 @@ setInterval(function() {
       port: args[4],
       path: '/right?name=' + args[5],
       method: 'GET'
-    }, function(data) {});
+    }, function(data) {
+      if (data == 'join the server to move') {
+        join();
+      }
+    });
   }
   if (movement == 4) {
     console.log('Shooting at target...');
@@ -60,7 +79,11 @@ setInterval(function() {
           port: args[4],
           path: '/shoot?name=' + encodeURI(args[5]) + '&x=' + serverdata[args[6]].x + '&y=' + serverdata[args[6]].y,
           method: 'GET'
-        }, function(data) {});
+        }, function(data) {
+          if (data == 'join the server to shoot') {
+            join();
+          }
+        });
       }
       else {
         console.log('Target not found.');
