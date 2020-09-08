9abf7dab8d451e06e8580144319fbce32c90d0ba
Lennon McLean <lmmclean08@gmail.com>
2020-09-07 20:14:29 -0400

    changed how the whole thing is packaged

--- main.js
+++ main.js
@@ -2,27 +2,6 @@
 if (process.argv[2] == 'server') {
   require('./server.js');
 }
-else if (process.argv[2] == 'client') {
-  var gui = require('@thecoder08/gui');
-  var path = require('path');
-  gui.menu([
-    {
-      label: 'node',
-      submenu: [
-        {
-          label: 'Quit node',
-          accelerator: 'CmdOrCtrl+Q',
-          onClick: process.exit
-        }
-      ]
-    }
-  ]);
-  gui.win(path.join('file://', __dirname, 'index.html'), {
-    height: 1000,
-    width: 1000
-  });
-  gui.start();
-}
 else if (process.argv[2] == 'bot') {
   require('./bot.js');
 }
